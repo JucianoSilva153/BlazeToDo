6fd60784a5c7b447eb8302d373fa9a5d3031e382
JucianoSilva153 <jucianodasilvasilvaa@gmail.com>
2023-12-24 23:32:11 +0100

    feat: adicionado a parte introdutoria do site

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,razor}"],
+  theme: {
+    extend: {},
+    colors:{
+      "Primaria": "#5688C7",
+      "Secundaria": "#0D2149",
+      "Neutra": "#FFFFFFFF"
+    }
+  },
+  plugins: [],
+}
+
